beccde7e21b5cd42d5af9d533642f47a2dda7848
pjha1994 <pjha1994@rediffmail.com>
2017-07-26 23:24:35 +0530

    msg

---
+++ Hello/src/main/java/com/Jha/How/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.Jha.How;
+
+public class HelloWorld{
+	static {
+		for(int i=0;i<5;i++)
+			System.out.println("How are you? hello World " + i);
+	}
+	public static void main(String[] args) {}
+}
