1522590b75c11538d94a312576cc3bc3beb85217
pjha1994 <pjha1994@rediffmail.com>
2017-08-15 16:29:15 +0530

    app.java

--- Hello/src/main/java/com/abhi/Hello/App.java
+++ Hello/src/main/java/com/abhi/Hello/App.java
@@ -6,8 +6,12 @@ package com.abhi.Hello;
  */
 public class App 
 {
+    /**
+     * @param args
+     */
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        for(int i=0;i<=100;i++)
+            System.out.println( "Hello World! "+ i );
     }
 }
