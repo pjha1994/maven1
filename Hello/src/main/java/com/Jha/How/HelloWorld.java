4760df9555ced22e15e04c588de6dd9a31166aaa
pjha1994 <pjha1994@rediffmail.com>
2017-09-30 14:53:47 +0530

    decimal to any base converter

--- Hello/src/main/java/com/Jha/How/HelloWorld.java
+++ Hello/src/main/java/com/Jha/How/HelloWorld.java
@@ -5,5 +5,18 @@ public class HelloWorld{
 		for(int i=0;i<5;i++)
 			System.out.println("How are you? hello World " + i);
 	}
-	public static void main(String[] args) {}
+	public static void main(String[] args) {
+		System.out.println(Converter(31,2));
+	}
+	private static int Converter(int num, int base){
+		int s = 0;
+		int p =1;
+		while(num>0){
+			int temp = num%base;
+			num = num/base;
+			s = s  + temp*p;
+			p = p*10;
+		}
+		return s;
+	}
 }
